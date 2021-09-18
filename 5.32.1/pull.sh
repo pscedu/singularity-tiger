fbc8e96859161d5b7516fe45aaae85c5605cfa4c
icaoberg <icaoberg@andrew.cmu.edu>
2021-09-17 21:54:15 -0400

    Update pull.sh

--- 5.32.1/pull.sh
+++ 5.32.1/pull.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Copyright © 2021 Pittsburgh Supercomputing Center.
+# All Rights Reserved.
+
 singularity pull https://depot.galaxyproject.org/singularity/bamtools:2.5.1--he513fc3_6
 mv -v bamtools:2.5.1--he513fc3_6 singularity-bamtools-2.5.1.sif
